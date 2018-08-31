c96ca2a2d43f329baa7cee9fbec94b9616cff780
Martin <mpicollo.utn@gmail.com>
2018-08-30 21:06:14 -0300

    Fix user test

--- src/test/java/com/utn/tacs/eventmanager/UserControllerTest.java
+++ src/test/java/com/utn/tacs/eventmanager/UserControllerTest.java
@@ -1,10 +1,13 @@
 package com.utn.tacs.eventmanager;
 
+import com.google.gson.Gson;
 import com.utn.tacs.eventmanager.controllers.UserController;
+import com.utn.tacs.eventmanager.controllers.dto.UserDTO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -20,7 +23,13 @@ public class UserControllerTest {
 
 	@Test
 	public void shouldCreateUser() throws Exception {
-		mockMvc.perform(post("/users")).andExpect(status().isCreated());
+		UserDTO user = new UserDTO();
+		user.setUsername("Martin");
+		user.setPassword("123456789");
+
+		mockMvc.perform(post("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new Gson().toJson(user))).andExpect(status().isCreated());
 	}
 
 }
