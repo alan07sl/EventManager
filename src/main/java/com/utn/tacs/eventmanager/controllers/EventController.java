c74073c5097c1c2844ee45a6ed1a982f90467352
Federico <romanofederico5@gmail.com>
2018-09-02 17:28:14 -0300

    Endpoints
    /users/{id} GET
    /events GET
    /users GET

--- src/main/java/com/utn/tacs/eventmanager/controllers/EventController.java
+++ src/main/java/com/utn/tacs/eventmanager/controllers/EventController.java
@@ -3,10 +3,7 @@ package com.utn.tacs.eventmanager.controllers;
 import com.utn.tacs.eventmanager.controllers.dto.EventUsersDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
@@ -18,5 +15,10 @@ public class EventController {
 		usersInterested.setAmount(1);
 		return new ResponseEntity<>(usersInterested,HttpStatus.OK);
 	}
+
+	@GetMapping
+    public ResponseEntity<String> getEvents(String eventos){
+        return new ResponseEntity<String>("Eventos", HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/utn/tacs/eventmanager/controllers/UserController.java
+++ src/main/java/com/utn/tacs/eventmanager/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.utn.tacs.eventmanager.controllers;
 import com.utn.tacs.eventmanager.controllers.dto.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,5 +13,19 @@ public class UserController {
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody UserDTO user) {
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getUsers(UserDTO user) {
+        return new ResponseEntity<String>("users", HttpStatus.OK);
+    }
+
+    @RequestMapping("/{id}")
+    @GetMapping
+    public @ResponseBody
+    ResponseEntity<String>
+    getById(@PathVariable String id) {
+        return new ResponseEntity<String>("GET ID : "
+                + id, HttpStatus.OK);
     }
 }
