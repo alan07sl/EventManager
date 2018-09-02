39977207ce4a8c2655452795bdc3c8c46fdddb22
Alan Hryniewicz <alan.07-sl@hotmail.com>
2018-09-02 13:17:04 -0300

    Alarms and event users (#8)
    * Alarm creating endpoint + test
    
    * Get ammount of users interested in a specific event endpoint + test
    
    * Change package of UserControllerTest
    
    * Fix typo

---
+++ src/main/java/com/utn/tacs/eventmanager/controllers/EventController.java
@@ -0,0 +1,22 @@
+package com.utn.tacs.eventmanager.controllers;
+
+import com.utn.tacs.eventmanager.controllers.dto.EventUsersDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+	@GetMapping("/{eventId}/users")
+	public ResponseEntity<EventUsersDTO> getEventUsers(@PathVariable Integer eventId) {
+		EventUsersDTO usersInterested = new EventUsersDTO();
+		usersInterested.setAmount(1);
+		return new ResponseEntity<>(usersInterested,HttpStatus.OK);
+	}
+
+}
---
+++ src/test/java/com/utn/tacs/eventmanager/controllers/EventControllerTest.java
@@ -0,0 +1,29 @@
+package com.utn.tacs.eventmanager.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(EventController.class)
+public class EventControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldGet() throws Exception {
+
+		mockMvc.perform(get("/events/1/users"))
+				.andExpect(jsonPath("$.amount").isNumber())
+				.andExpect(status().isOk());
+	}
+
+}
