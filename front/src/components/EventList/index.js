00342de7ce46f4ef0b6fbf511d25d4ef45f44417
Alan Halatian <alanhalatian@gmail.com>
2018-10-16 01:24:39 -0300

    added style for each event

--- front/src/components/EventList/index.js
+++ front/src/components/EventList/index.js
@@ -1,31 +1,16 @@
-import React from 'react';
-import { connect } from 'redux-zero/react';
-import withAuth from '../withAuth';
-import './styles.css';
-import SearchList from '../SearchList';
-import LinkIcon from '@material-ui/icons/Link';
-import Button from '@material-ui/core/Button';
-import Clipboard from 'react-clipboard.js';
-import popupService from '../../services/popupService';
+import React from "react";
+import { connect } from "redux-zero/react";
+import withAuth from "../withAuth";
+import "./styles.css";
+import SearchList from "../SearchList";
+import Event from "../Event";
 
 class EventList extends React.Component {
-  
-  copyClipboard = () => popupService.successPopup('EventID copied to clipboard');
-  
   render() {
     return (
       <SearchList
-        nameService={'getEvents'}
-        displayItem={item => (
-          <div className="list-row" key={item.id}>
-            <a className="item">{item.name.text}</a>
-            <Clipboard data-clipboard-text={item.id} className="link-button">
-              <Button aria-label="Link" onClick={this.copyClipboard}>
-                <LinkIcon />
-              </Button>
-            </Clipboard>
-          </div>
-        )}
+        nameService={"getEvents"}
+        displayItem={item => <Event key={item.id} event={item} />}
       />
     );
   }
